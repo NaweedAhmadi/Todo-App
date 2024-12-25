02f471f95707415014befccc6e38d74ddcaa789c
Naweed Ahmadi <naweed.ahmadi2010@gmail.com>
2024-12-25 00:52:29 -0800

    Sixth Sub Commit - Added the pnpm-lock.yaml file.

--- api/server.js
+++ api/server.js
@@ -1,28 +1,31 @@
 // See https://github.com/typicode/json-server#module
-const jsonServer = require('json-server');
-const fs = require('fs');
-const path = require('path');
-const filePath = path.join('db.json');
+const jsonServer = require('json-server')
+
+const server = jsonServer.create()
+
+// Uncomment to allow write operations
+const fs = require('fs')
+const path = require('path')
+const filePath = path.join('db.json')
 const data = fs.readFileSync(filePath, "utf-8");
 const db = JSON.parse(data);
-const server = jsonServer.create();
+const router = jsonServer.router(db)
 
-// Use the data read from db.json for the router
-const router = jsonServer.router(db);
+// Comment out to allow write operations
+// const router = jsonServer.router('db.json')
 
-const middlewares = jsonServer.defaults();
+const middlewares = jsonServer.defaults()
 
-server.use(middlewares);
-
-// Rewriting URLs to match the API structure
+server.use(middlewares)
+// Add this before server.use(router)
 server.use(jsonServer.rewriter({
-    '/api/*': '/$1', // Rewrite /api/* routes to match db.json
-}));
-
-server.use(router);
+    '/api/*': '/$1',
+    '/blog/:resource/:id/show': '/:resource/:id'
+}))
+server.use(router)
 server.listen(3000, () => {
-    console.log('JSON Server is running');
-});
+    console.log('JSON Server is running')
+})
 
 // Export the Server API
-module.exports = server;
+module.exports = server
